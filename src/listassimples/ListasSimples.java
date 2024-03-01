58dfcac016f688e32c5b5275a8fbb1d95f482cf8
jhoriv <jhoriv@GCP26614.gascaribe.local>
2024-03-01 14:50:33 -0500

    Metodo Buscar

--- src/listassimples/ListasSimples.java
+++ src/listassimples/ListasSimples.java
@@ -8,12 +8,14 @@ import javax.swing.JOptionPane;
 
 class Estudiante {
 
+    int id;
     String nombre;
     int edad;
 
-    public Estudiante(String nombre, int edad) {
+    public Estudiante(int id, String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
+        this.id = id;
     }
 
 }
@@ -42,6 +44,7 @@ class Lista {
     //Metodo Insertar
     public void insertarInicio(Estudiante estudiante) {
         Nodo n = new Nodo(estudiante);
+
         if (L == null) {
             L = n;
         } else {
@@ -50,12 +53,40 @@ class Lista {
         }
     }
 
+    public boolean existe(int id) {
+        Nodo p = L;
+        while (p != null) {
+            if (p.estudiante.id == id) {
+                System.out.println(p.estudiante.id);
+                return true;
+            }
+
+            p = p.sig;
+        }
+        return false;
+    }
+
     // Metodo Imprimir
     public void imprimir() {
         String cad = "";
         Nodo p = L;
         while (p != null) {
-            System.out.println(p.estudiante.nombre + "- Edad: " + p.estudiante.edad);
+            System.out.println("ID: " + p.estudiante.id + "Nombre: " + p.estudiante.nombre + "- Edad: " + p.estudiante.edad);
+            p = p.sig;
+        }
+        System.out.println(cad);
+    }
+
+    // imprimir por ID
+    public void imprimirID(int id) {
+        String cad = "";
+        Nodo p = L;
+        while (p != null) {
+
+            if (id == p.estudiante.id) {
+                System.out.println("ID" + p.estudiante.id + " - Nombre: " + p.estudiante.nombre + "- Edad: " + p.estudiante.edad);
+            }
+
             p = p.sig;
         }
         System.out.println(cad);
@@ -66,6 +97,7 @@ class Lista {
         Nodo p = L;
         double suma = 0;
         while (p != null) {
+
             suma += p.estudiante.edad;
             p = p.sig;
         }
@@ -113,26 +145,50 @@ public class ListasSimples {
                     + "6. Salir del Progrma"));
             switch (opc) {
                 case 1:
-                    String nombre = JOptionPane.showInputDialog("Digite Nombre estudiante");
-                    int ed = Integer.parseInt(JOptionPane.showInputDialog("Digite Edad"));
-                    Estudiante est = new Estudiante(nombre, ed);
-                    obj.insertarInicio(est);
 
-                    JOptionPane.showMessageDialog(null, "Estudiante Agregado");
+                    int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id del estudiante"));
+                    boolean ex = obj.existe(id);
+
+                    System.out.println(obj.existe(id));
+                    if (ex) {
+
+                        System.out.println("Estudinate ya existe");
+
+                    } else {
+                        String nombre = JOptionPane.showInputDialog("Digite Nombre estudiante");
+                        int ed = Integer.parseInt(JOptionPane.showInputDialog("Digite Edad"));
+                        Nodo L;
+                        Estudiante est = new Estudiante(id, nombre, ed);
+                        obj.insertarInicio(est);
+                        JOptionPane.showMessageDialog(null, "Estudiante Agregado");
+
+                    }
 
                     break;
 
                 case 2:
                     obj.imprimir();
                     break;
-                    
+
                 case 3:
-                    
-                        System.out.println("Promedio de edades: " + obj.promedioEdades());
+
+                    System.out.println("Promedio de edades: " + obj.promedioEdades());
                     break;
-                    
+
+                case 5:
+                    int idBusc = Integer.parseInt(JOptionPane.showInputDialog("Digite id a buscar"));
+                    boolean existeE = obj.existe(idBusc);
+                    if (existeE) {
+                        obj.imprimirID(idBusc);
+
+                    }else{
+                        System.out.println("No hay informacion de ese ID");
+                    }
+
+                    break;
+
                 default:
-                    throw new AssertionError();
+                    JOptionPane.showMessageDialog(null, "Opcion incorrecta");
             }
 
         } while (opc != 6);
